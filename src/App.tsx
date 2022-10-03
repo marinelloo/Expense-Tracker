e282cf3d1ea212d53bad2ba4181af1b27c581dbf
marinelloo <maryna.chertkoieva@gmail.com>
2022-10-03 21:49:26 +0200

    setup project

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styled from 'styled-components'
+import image from './assets/images/test-image.jpg'
+import ClickCounter from './ClickCounter'
+
+const AppStyled = styled.div`
+  color: red;
+  display: flex;
+`
+
+export const App = () => {
+  const num = "name"
+  return (
+    <AppStyled>
+      <img src={image} alt="test" />
+      <h1>
+        {' '}
+        Edit5 React TypeScript Webpack Starter Template - {process.env.NODE_ENV}
+      </h1>
+      <ClickCounter />
+    </AppStyled>
+  )
+}
